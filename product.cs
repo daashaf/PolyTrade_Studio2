86b83e4083bdc5670e5c293705cd10f6aa307ab9
viren-g <virengollen2004@gmail.com>
2025-08-28 19:15:03 +1200

    add a new class product

---
+++ product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyTrade_WebApp
+{
+    internal class product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            // This will display nicely in ListBox
+            return $"{Name}   ${Price}";
+        }
+    }
+}
